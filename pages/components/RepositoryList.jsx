07a5c0eb4249298026d76033db8f451260fff0dd
Gilbertoaleite <gilbertoaleite@hotmail.com>
2022-02-16 23:13:42 -0300

    Implemetando hooks e api

--- pages/components/RepositoryList.jsx
+++ pages/components/RepositoryList.jsx
@@ -1,6 +1,10 @@
 /** @format */
 
+// import styles from '../styles/repositories.scss'; 
+import { useState, useEffect } from 'react';
 import { RepositoryItem } from './RepositoryItem';
+
+//https://api.github.com/users/Gilbertoaleite/repos
 
 const repository = {
 	name: 'unform',
@@ -9,6 +13,14 @@ const repository = {
 }
 
 export function RepositoryList() {
+	const [repositories, setRepositories] = useState([]);
+	
+	useEffect(() => {
+		fetch('https://api.github.com/users/Gilbertoaleite/repos')
+			.then(response => response.json())
+		.then(data => setRepositories(data)) //console.log(data))
+	}, []); //nunca deixar o userEffect sem atributo vazio para nao dar loop infinito, se não for colocar colocar [] vazio
+	
 	return (
 		<section className='repository-list'>
 			<h1>Lista de repositórios</h1>
